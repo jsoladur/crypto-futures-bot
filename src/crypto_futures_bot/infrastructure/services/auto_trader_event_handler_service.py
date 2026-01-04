38323f38a7c40637ab3efc648c9a0e0685467a3d
José María Sola Durán <josemaria.sola.duran@gmail.com>
2026-01-04 17:30:20 +0100

    Passing events for event signals

---
+++ src/crypto_futures_bot/infrastructure/services/auto_trader_event_handler_service.py
@@ -0,0 +1,45 @@
+import asyncio
+import logging
+from typing import override
+
+from pyee.asyncio import AsyncIOEventEmitter
+
+from crypto_futures_bot.config.configuration_properties import ConfigurationProperties
+from crypto_futures_bot.constants import MARKET_SIGNAL_EVENT_NAME
+from crypto_futures_bot.domain.vo import MarketSignalItem
+from crypto_futures_bot.infrastructure.services.base import AbstractEventHandlerService
+from crypto_futures_bot.infrastructure.services.push_notification_service import PushNotificationService
+from crypto_futures_bot.infrastructure.services.trade_now_service import TradeNowService
+from crypto_futures_bot.interfaces.telegram.services.telegram_service import TelegramService
+
+logger = logging.getLogger(__name__)
+
+
+class MarketSignalService(AbstractEventHandlerService):
+    def __init__(
+        self,
+        configuration_properties: ConfigurationProperties,
+        push_notification_service: PushNotificationService,
+        telegram_service: TelegramService,
+        event_emitter: AsyncIOEventEmitter,
+        trade_now_service: TradeNowService,
+    ) -> None:
+        super().__init__(push_notification_service, telegram_service, event_emitter)
+        self._configuration_properties = configuration_properties
+        self._trade_now_service = trade_now_service
+        self._lock = asyncio.Lock()
+
+    @override
+    def configure(self) -> None:
+        self._event_emitter.add_listener(MARKET_SIGNAL_EVENT_NAME, self._handle_signals_evaluation_result)
+
+    async def on_open_position_from_market_signal(self, market_signal_item: MarketSignalItem) -> None:
+        async with self._lock:
+            try:
+                await self._internal_on_open_position_from_market_signal(market_signal_item)
+            except Exception as e:  # pragma: no cover
+                logger.error(str(e), exc_info=True)
+                await self._notify_fatal_error_via_telegram(e)
+
+    async def _internal_on_open_position_from_market_signal(self, market_signal_item: MarketSignalItem) -> None:
+        raise NotImplementedError("To be implemented!")
